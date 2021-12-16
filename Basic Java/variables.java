cc0fae4416a96b853e5ca4d70feed9ec228cdf71
eddybruv <edwinajong@gmail.com>
2021-12-16 20:52:40 +0100

    variable types and declaration

---
+++ Basic Java/variables.java
@@ -0,0 +1,12 @@
+// package Basic Java;
+
+public class variables {
+   String name = "Edwin Ajong";
+   int age = 20;
+   float weight = 50.6f;
+   char sex = 'M'; 
+   boolean alive = true;
+
+   //final keyword is used as constant keyword.
+   final int value = 1;
+}
