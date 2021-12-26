90d4d411205b4b1c1fc5c8d1823c423c42db535c
eddybruv <edwinajong@gmail.com>
2021-12-26 20:36:08 +0100

    char type in java

---
+++ Chapter 3/char.java
@@ -0,0 +1,13 @@
+//Demonstrate char data type
+
+class CharDemo {
+    public static void main(String args[]){
+        char ch1, ch2;
+
+        ch1 = 87; // code for W
+        ch2 = 'X';
+
+        System.out.print("ch1 and ch2: ");
+        System.out.println(ch1 + " " + ch2);
+    }
+}
